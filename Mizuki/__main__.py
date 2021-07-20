2b531d7bf666b93a40e0870f136218222d230aff
Ez-Tee <81809747+EZ-Tee@users.noreply.github.com>
2021-07-20 13:23:54 +0530

    Update __main__.py

--- Mizuki/__main__.py
+++ Mizuki/__main__.py
@@ -95,7 +95,7 @@ I'm a group management bot, here to help you get around and keep the order in yo
     "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !\n",
 )
 
-SAITAMA_IMG = "https://telegra.ph/file/998702892e07473f3e6e6.jpg"
+SAITAMA_IMG = "https://telegra.ph/file/af86973849bc43cc8e3ce.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate for developer. This bot runs on heroku so bot slow down some times and developer cannot add more modules due to heroku can't run them.\n\nBetter if my developer recieved a VPS to run the bot. Contact him and help him to continue this.\n\nDeveloper: [@ImJanindu](t.me/imjanindu)"""
 
