a169fd5463ac7e7ac1f5185cbde35fff151d113c
Ez-Tee <81809747+EZ-Tee@users.noreply.github.com>
2021-08-05 12:05:25 +0530

    Update __main__.py

--- Mizuki/__main__.py
+++ Mizuki/__main__.py
@@ -205,8 +205,7 @@ def start(update: Update, context: CallbackContext):
             first_name = update.effective_user.first_name
             update.effective_message.reply_sticker(
                 SAITAMA_IMG,
-                update.effective_message.reply_photo(
-                SAITAMA_IMG,
+                
                 PM_START_TEXT.format(
                     escape_markdown(first_name), escape_markdown(context.bot.first_name)
                 ),
